ba12da927889a2545fd82d86cb6d2c405613177d
joshloblaw <josh.alexander01@loblaw.ca>
2022-09-20 10:39:21 -0400

    cleaning up folder

--- Sustainability/process.py
+++ Sustainability/process.py
@@ -1,69 +1,109 @@
 import pandas as pd
 from fuzzywuzzy import process, fuzz
+import sys
 
+# MASTER FILE VAR
+MPATH = "NationalStoreList.xlsm"
+MSHEET = [2]
+MHEAD = 11
+MCOLS = [9, 10, 11, 12]
+MNROWS = 1066
 
-# FILE NAME/PATH
-MASTER_PATH = "Facilities.xlsx"
-TARGET_PATH = "CustomReport.xlsx"
-OUTPUT_NAME = "Output_" + TARGET_PATH
+# TARGET FILE VAR
+TPATH = "CustomReport2021_v2.xlsx"
+TSHEET = [0]
+THEAD = 0
+TCOLS = [0, 1, 2, 3, 17, 24]
 
-# SHEETS (0-indexed)
-MASTER_SHEET = [0]
-TARGET_SHEET = [0]
+# HEADER
+ID = "ID"
+ADDR = "Address"
+SN = "StoreName"
+SC = "City"
 
-# HEADER LOCATION (0-indexed)
-MASTER_HEADER = 0
-TARGET_HEADER = 6
-
-# HEADER NAME
-MASTER_REF = "Address: Street 1"
-MASTER_NUM = "Name"
-
-TARGET_REF = "Address "
-TARGET_NUM = "Store Number"
-TARGET_ID = "EPL Identifier "
-
-# ----------------------------------------------#
 # READING
-# ----------------------------------------------#
-mdict_df = pd.concat(pd.read_excel(MASTER_PATH, sheet_name=MASTER_SHEET, header=MASTER_HEADER), ignore_index=True)
-tdict_df = pd.concat(pd.read_excel(TARGET_PATH, sheet_name=TARGET_SHEET, header=TARGET_HEADER), ignore_index=True)
+master = pd.concat(pd.read_excel(MPATH, sheet_name=MSHEET, header=MHEAD, nrows=MNROWS, usecols=MCOLS), ignore_index=True)
+target = pd.concat(pd.read_excel(TPATH, sheet_name=TSHEET, header=THEAD, usecols=TCOLS), ignore_index=True)
 
-# ----------------------------------------------#
 # MATCHING
-# ----------------------------------------------#
-store_numbers = []
-master_addresses = mdict_df[MASTER_REF]
-prev_epl = -1
-current_epl = -1
-store_number = -1
+sids = []
+ref_addr = []
+ref_sn = []
+accs = []
+cities = []
+addr_book = master[ADDR]
+sn_book = master[SN]
+sn_book_lower = [ x.lower() for x in sn_book ]
 
-for i in range(len(tdict_df)):
-    current_epl = tdict_df[TARGET_ID][i]
-    if store_number != -1 and current_epl == prev_epl:
-        store_numbers.append(store_number)
+count, prev_sn, sn, sid, addr, acc, city = 0, 0, 0, 0, 0, 0, 0
+
+for row in range (len(target)):
+
+    if row % 1500 == 0:
+        print(row)
+
+    t_sn = target[SN][row]
+
+    if t_sn == -1:
+        sids.append(-1)
+        ref_sn.append(-1)
+        ref_addr.append(-1)
+        accs.append(-1)
+        cities.append(-1)
+        continue
+    else:
+        try:
+            t_sn.lower()
+        except AttributeError:
+            print(t_sn, row)
+            sys.exit(1)
+
+
+    if t_sn == prev_sn:
+
+        if (sid != -1):
+            count += 1
+
+        sids.append(sid)
+        ref_sn.append(sn)
+        ref_addr.append(addr)
+        accs.append(acc)
+        cities.append(city)
+
         continue
 
-    print(current_epl)
-    t_address = tdict_df[TARGET_REF][i]
-    m_address = process.extractOne(t_address, master_addresses, scorer=fuzz.partial_ratio, score_cutoff=89)
-    
-    if (m_address):
-        m_index = m_address[2]
-        store_number = mdict_df[MASTER_NUM][m_index]
+    m_sn = process.extractOne(t_sn, sn_book, scorer=fuzz.ratio, score_cutoff=100)
+
+    if (m_sn):
+        sn = m_sn[0]
+        acc = m_sn[1]
+        addr = master[ADDR][m_sn[2]]
+        sid = master[ID][m_sn[2]]
+        city = master[SC][m_sn[2]]
+        count += 1
     else:
-        store_number = -1
+        sn = -1
+        acc = -1
+        addr = -1
+        sid = -1
+        city = -1
 
-    store_numbers.append(store_number)
-    prev_epl = current_epl
+    sids.append(sid)
+    ref_sn.append(sn)
+    ref_addr.append(addr)
+    accs.append(acc)
+    cities.append(city)
 
-# ----------------------------------------------#
+    prev_sn = t_sn
+
 # OUTPUT
-# ----------------------------------------------#
-tdict_df.insert(0, TARGET_NUM, store_numbers)
+target.insert(0, "REF_CITY", cities)
+target.insert(0, "REF_SN", ref_sn)
+target.insert(0, "REF_ADDR", ref_addr)
+target.insert(0, "ACCURACY", accs)
+target.insert(0, "SID", sids)
 
-with pd.ExcelWriter(OUTPUT_NAME) as writer:
-    tdict_df.to_excel(excel_writer=writer, sheet_name="Electricity", startrow=7, startcol=1)
+with pd.ExcelWriter("output.xlsx") as writer:
+    target.to_excel(excel_writer=writer, sheet_name="2021 Electricity")
 
-print("Completed")
-    
+print ("Completed | Count: ", count)
